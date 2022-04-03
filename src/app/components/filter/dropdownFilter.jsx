a98bca71800a5112456a809195fcad6c76aee8e9
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-04-03 15:37:46 -0500

    Dropdown filter desgin

--- src/app/components/filter/dropdownFilter.jsx
+++ src/app/components/filter/dropdownFilter.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const dropdownFilter = () => {
-  return (
-    <div>dropdownFilter</div>
-  )
-}
+  const mode = useSelector((state) => state.darkMode);
+  const regions = [
+    { value: "Africa", label: "Africa" },
+    { value: "América", label: "América" },
+    { value: "Asia", label: "Asia" },
+    { value: "Europe", label: "Europe" },
+    { value: "Oceania", label: "Oceania" },
+  ];
 
-export default dropdownFilter
+  return (
+    <div className="dropdownFilter">
+      <select
+        className={
+          mode
+            ? "dropdownFilter_select dropdownFilter_select-dark"
+            : "dropdownFilter_select dropdownFilter_select-light"
+        }
+        defaultValue=""
+      >
+        <option value="" disabled hidden>
+          Filter by Region
+        </option>
+        {regions.map((region, id) => (
+          <option key={id} value={region.value}>
+            {region.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default dropdownFilter;
