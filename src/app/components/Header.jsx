3a57b6f1e4b8944eeef528feb29e443570ed3086
Nicolas Sierra <nico.sierra13@gmail.com>
2022-04-03 11:40:31 -0500

    Button states Light and Dark mode

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
 import { faMoon as faMoonSolid}  from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
+  const[click, setClick]=useState(false)
+  const changeClick =  () =>{
+    setClick(!click)
+}
   return (
     <div className="header">
         <div className="header-container container">
@@ -11,8 +15,17 @@ const Header = () => {
                 <h1 className="header-title">Where in the world?</h1>
             </div>
             <div className="header-btn">
-                <FontAwesomeIcon icon={faMoon} /> 
-                <button className="header-mode"> Dark Mode </button>
+           
+                {click ? 
+                  <FontAwesomeIcon icon={faMoon} />: 
+                  <FontAwesomeIcon icon={faMoonSolid}/>
+                }
+                
+                <button className="header-mode"
+                  onClick={() => changeClick()}
+                > 
+                  {click?`Light Mode`:`Dark Mode`}
+                </button>
             </div>
         </div>
     </div>
