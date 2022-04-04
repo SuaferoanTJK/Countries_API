c69e050e08e7740765a49a4004253788231ca08f
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-04-03 19:59:13 -0500

    useState not required

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,5 +1,4 @@
-
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-regular-svg-icons";
 import { faMoon as faMoonSolid } from "@fortawesome/free-solid-svg-icons";
@@ -9,27 +8,30 @@ import { changeMode } from "../redux/actions/countriesActions";
 const Header = () => {
   const dispatch = useDispatch();
   const mode = useSelector((state) => state.darkMode);
-  const [click, setClick] = useState(mode);
 
   const changeClick = () => {
-    setClick(!click);
-    dispatch(changeMode(!click));
+    dispatch(changeMode(!mode));
   };
 
   return (
-    <div className={click ? "header header_bg-dark" : "header header_bg-light"}>
+    <div className={mode ? "header header_bg-dark" : "header header_bg-light"}>
       <div className="header-container container">
         <div className="header-logo">
           <h1 className="header-title">Where in the world?</h1>
         </div>
         <div className="header-btn">
-          {click ? (
+          {mode ? (
             <FontAwesomeIcon icon={faMoonSolid} />
           ) : (
             <FontAwesomeIcon icon={faMoon} />
           )}
-          <button className={click ? "header-mode text-white" : "header-mode text-dark"} onClick={() => changeClick()}>
-              {click ? "Dark Mode" : "Light Mode"}
+          <button
+            className={
+              mode ? "header-mode text-white" : "header-mode text-dark"
+            }
+            onClick={() => changeClick()}
+          >
+            {mode ? "Dark Mode" : "Light Mode"}
           </button>
         </div>
       </div>
